4bbe558e14e43547204aa32f49acd805b4db107f
BERIM-amdjar <m.amdjar@gmail.com>
2022-04-26 10:17:57 +0200

    Voo

---
+++ qgis2web_2022_04_22-08_51_59_041000/data/Info_4.js
@@ -0,0 +1,13 @@
+var json_Info_4 = {
+"type": "FeatureCollection",
+"name": "Info_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Info": "Armoire devant la façade à déplacer, intégration au bâtiment à priori n'est pas possible", "Suivi": null }, "geometry": { "type": "Point", "coordinates": [ 2.403298350415083, 48.8164170136602 ] } },
+{ "type": "Feature", "properties": { "Info": "Démontage de la base vie à partir du fin juin 2022. projet d'aménagement provisoire de la place à définir !", "Suivi": null }, "geometry": { "type": "Point", "coordinates": [ 2.402969578500173, 48.815770804790425 ] } },
+{ "type": "Feature", "properties": { "Info": "Démontage de l'école provisoire à partir du 15 juillet 2022 pour une durée de 3 semaines cis GC et déraccordement électrique", "Suivi": null }, "geometry": { "type": "Point", "coordinates": [ 2.402440281649607, 48.815514739351421 ] } },
+{ "type": "Feature", "properties": { "Info": "Poteau incendie existant hors service !", "Suivi": null }, "geometry": { "type": "Point", "coordinates": [ 2.397914253478446, 48.812360048090788 ] } },
+{ "type": "Feature", "properties": { "Info": "Programmation des travaux des espaces publics et des études à faire", "Suivi": null }, "geometry": { "type": "Point", "coordinates": [ 2.398178901903728, 48.812655253098569 ] } },
+{ "type": "Feature", "properties": { "Info": "Attention, il faut prévoir un poste DP", "Suivi": null }, "geometry": { "type": "Point", "coordinates": [ 2.402415977202387, 48.813384366452745 ] } }
+]
+}
