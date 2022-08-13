5b4c9580b8d8e70d57b2b4dd966aedaad3a5c422
BERIM-amdjar <76733904+BERIM-amdjar@users.noreply.github.com>
2022-08-14 00:45:04 +0200

    test

--- data/GeoCodingPluginResults_4.js
+++
@@ -1,8 +0,0 @@
-var json_GeoCodingPluginResults_4 = {
-"type": "FeatureCollection",
-"name": "GeoCodingPluginResults_4",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-
-]
-}
