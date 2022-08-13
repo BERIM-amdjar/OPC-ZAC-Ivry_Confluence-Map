0aaf7cc1b6ff6eb08aaeb92097f036b71e7c1d1c
BERIM-amdjar <76733904+BERIM-amdjar@users.noreply.github.com>
2022-08-13 23:01:55 +0200

    VN1

--- data/Travauxconstruction_2.js
+++ data/Travauxconstruction_2.js
@@ -6,11 +6,10 @@ var json_Travauxconstruction_2 = {
 { "type": "Feature", "properties": { "Nom LOT": "Lot 1A", "Date PC": "Octobre 2020", "Début": "Demolition 2022", "Livraison": "-" }, "geometry": { "type": "Point", "coordinates": [ 2.390065482801304, 48.816491926106075, 0.0 ] } },
 { "type": "Feature", "properties": { "Nom LOT": "1B ENERIGE", "Date PC": "xxx", "Début": "T1 2021 (demolition)", "Livraison": null }, "geometry": { "type": "Point", "coordinates": [ 2.391881592373426, 48.816346199230736, 0.0 ] } },
 { "type": "Feature", "properties": { "Nom LOT": "2L SIRIUS (??)", "Date PC": "xxx", "Début": "T4 - 2020", "Livraison": "T4 - 2021" }, "geometry": { "type": "Point", "coordinates": [ 2.394166052171559, 48.815561927547648, 0.0 ] } },
-{ "type": "Feature", "properties": { "Nom LOT": "1E2a", "Date PC": "xxx", "Début": "T1 - 2021", "Livraison": "T3 - 2023" }, "geometry": { "type": "Point", "coordinates": [ 2.392680853534797, 48.814945803226344, 0.0 ] } },
-{ "type": "Feature", "properties": { "Nom LOT": "2D2", "Date PC": "xxx", "Début": "T2 2021", "Livraison": "T1 2023" }, "geometry": { "type": "Point", "coordinates": [ 2.396375868917251, 48.816826825724888, 0.0 ] } },
+{ "type": "Feature", "properties": { "Nom LOT": "1E2a", "Date PC": "xxx", "Début": "?", "Livraison": "?" }, "geometry": { "type": "Point", "coordinates": [ 2.392680853534797, 48.814945803226344, 0.0 ] } },
+{ "type": "Feature", "properties": { "Nom LOT": "2D2", "Date PC": "xxx", "Début": "Avril 2022", "Livraison": "T2 2023" }, "geometry": { "type": "Point", "coordinates": [ 2.396375868917251, 48.816826825724888, 0.0 ] } },
 { "type": "Feature", "properties": { "Nom LOT": "3E", "Date PC": "xxx", "Début": "T2 2021", "Livraison": "Juin 2022" }, "geometry": { "type": "Point", "coordinates": [ 2.40244972723233, 48.816167432581928, 0.0 ] } },
 { "type": "Feature", "properties": { "Nom LOT": "2K1", "Date PC": "xxx", "Début": "Avril 2022", "Livraison": "A définir" }, "geometry": { "type": "Point", "coordinates": [ 2.398014716884334, 48.812709073826383, 0.0 ] } },
-{ "type": "Feature", "properties": { "Nom LOT": "5A1", "Date PC": "xxx", "Début": null, "Livraison": "14 avril 2022" }, "geometry": { "type": "Point", "coordinates": [ 2.398296101656929, 48.81206522449714, 0.0 ] } },
 { "type": "Feature", "properties": { "Nom LOT": "5C3", "Date PC": "xxx", "Début": null, "Livraison": "Juin 2023" }, "geometry": { "type": "Point", "coordinates": [ 2.40189217992123, 48.813752549991833, 0.0 ] } },
 { "type": "Feature", "properties": { "Nom LOT": "4C2", "Date PC": "xxx", "Début": "Decembre 2021", "Livraison": "Juillet 2022" }, "geometry": { "type": "Point", "coordinates": [ 2.403445452988854, 48.814367413778776, 0.0 ] } },
 { "type": "Feature", "properties": { "Nom LOT": "3J1", "Date PC": "xxx", "Début": "Nov 2020", "Livraison": "T3 - 2023" }, "geometry": { "type": "Point", "coordinates": [ 2.40327243822229, 48.815447410297402, 0.0 ] } },
@@ -18,7 +17,6 @@ var json_Travauxconstruction_2 = {
 { "type": "Feature", "properties": { "Nom LOT": "St Raphael", "Date PC": "xxx", "Début": "Dec 2020", "Livraison": "2023" }, "geometry": { "type": "Point", "coordinates": [ 2.40435215694259, 48.816571977985028, 0.0 ] } },
 { "type": "Feature", "properties": { "Nom LOT": "3G", "Date PC": "xxx", "Début": "T1 - 2021", "Livraison": "Mars 2023" }, "geometry": { "type": "Point", "coordinates": [ 2.404243839741317, 48.816350432032898, 0.0 ] } },
 { "type": "Feature", "properties": { "Nom LOT": "3O", "Date PC": "xxx", "Début": "T2 -2022", "Livraison": "Novembre 2022" }, "geometry": { "type": "Point", "coordinates": [ 2.406474626548601, 48.814898861326654, 0.0 ] } },
-{ "type": "Feature", "properties": { "Nom LOT": "4D2", "Date PC": "xxx", "Début": "Démolition 2021", "Livraison": "2025" }, "geometry": { "type": "Point", "coordinates": [ 2.408582367658352, 48.813881869594276, 0.0 ] } },
-{ "type": "Feature", "properties": { "Nom LOT": "5H2", "Date PC": "xxx", "Début": "T4 - 2020", "Livraison": "T1 - 2022" }, "geometry": { "type": "Point", "coordinates": [ 2.402790973478794, 48.810398180136787, 0.0 ] } }
+{ "type": "Feature", "properties": { "Nom LOT": "4D2", "Date PC": "xxx", "Début": "Fin 2022", "Livraison": "2025 ?" }, "geometry": { "type": "Point", "coordinates": [ 2.408582367658352, 48.813881869594276, 0.0 ] } }
 ]
 }
